9641bcb106a0d50c6b36e5045bfde4666a9c707c
Dumiya1111 <139087415+Dumiya1111@users.noreply.github.com>
2023-07-15 07:59:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,17 +21,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.owner = ['94771901094'] //ur owner number
+global.ownernomer = "94771901094" //ur owner number2
+global.ownername = "🦄MR | DUMIYA" //ur owner name
+global.ytname = "YT: DUMIYA" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
+global.ownernumber = '94771901094'
+global.ownername = '🦄MR | DUMIYA'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
